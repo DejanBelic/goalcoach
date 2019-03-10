d437141049d44b32f4e646fb24bf814c4e206d27
Dejan <dejan.belic@develomon.com>
2019-03-10 17:40:25 +0100

    Added 3 routes.

--- goalcoachcra/src/index.js
+++ goalcoachcra/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./Components/App";
+import { Router, Route, BrowserRouter } from 'react-router-dom';
 import SignIn from './Components/SignIn.jsx'
 import SignUp from './Components/SignUp.jsx';
 
 ReactDOM.render(
-  <App/> ,document.getElementById("root");
+  <BrowserRouter>
+    <>
+    <Route path="/app" component={App} />
+    <Route path="/signin" component={SignIn}/>
+    <Route path="/signup" component={SignUp}/>
+  </>
+  </BrowserRouter>, document.getElementById("root")
 );
