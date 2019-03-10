28e2e18f801627ab5a3d95fa3eefc0fb7794c7f7
Dejan <dejan.belic@develomon.com>
2019-03-10 18:41:21 +0100

    Added firebase to application, enabled in firebase console auth with email.

--- goalcoachcra/src/Components/SignUp.jsx
+++ goalcoachcra/src/Components/SignUp.jsx
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
+import { firebaseApp} from "../firebase";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: {
+        message: ''
+      }
+    }
+  }
+  signUp = () => {
+    const { email, password } = this.state;
+   firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+     .catch(error => {
+       this.setState({ error})
+     })
+  }
   render() {
     return (
-      <div>
-        Sign up
+      <div className="form-inline" style={{margin: '5%'}}>
+        <h2>Sign up</h2>
+        <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          style={{marginRight:'5%'}}
+          placeholder="email"
+          onChange={event => this.setState({ email: event.target.value })}
+        />
+          <input
+            className="form-control"
+            placeholder="password"
+            type="password"
+            style={{marginRight:'5%'}}
+            onChange={event => this.setState({ password: event.target.value })}
+          />
+          <button className="btn btn-primary" type="button"
+            onClick={this.signUp}
+          >Sign up</button>
+        </div>
+        <div>{this.state.error.message}</div>
       </div>
     );
   }
