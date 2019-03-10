6ed76704f8a5c0fba26dfeedfb042e518285a375
Dejan <dejan.belic@develomon.com>
2019-03-10 13:51:42 +0100

    Added react-router, redux react-redux firebase. Created App, signup and signin components.

---
+++ goalcoachcra/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./Components/App";
+import SignIn from './Components/SignIn.jsx'
+import SignUp from './Components/SignUp.jsx';
+
+ReactDOM.render(
+  <App/> ,document.getElementById("root");
+);
